d2820b21502f0d9b923becaa83a97cb6e3ca6226
Bruno Martin <bruno.martin@hacklab.com.br>
2017-04-14 20:47:38 -0300

    uber refactor que cria camada de informação transparente para eventos

--- js/mapao.js
+++ js/mapao.js
@@ -2,21 +2,38 @@ $( document ).ready(function() {
 
   L.mapbox.accessToken = 'pk.eyJ1IjoianVzdHRlc3RpbmciLCJhIjoiMEg3ZWJTVSJ9.h41984pPh9afTYWBg2eoQQ';
   var map = L.mapbox.map('map', 'mapbox.streets', {scrollWheelZoom: false})
-      .setView([  -22.946643, -43.058642], 14)
-      .on('click', function(e) {
-        map.scrollWheelZoom.enable();
-        map.dragging.enable();
-      });;
+      .setView([  -22.946643, -43.058642], 14);
+      // .on('click', function(e) {
+        // map.scrollWheelZoom.enable();
+        // map.dragging.enable();
+      // });;
       map.dragging.disable()
+
+      //  map.scrollWheelZoom.enable();
+      //  map.dragging.enable();
 
   // Use styleLayer to add a Mapbox style created in Mapbox Studio
   // L.mapbox.styleLayer('mapbox://styles/justtesting/cj1fp79gl00092rr091yoxrr6').addTo(map);
   L.mapbox.styleLayer('mapbox://styles/mapbox/streets-v10').addTo(map);
 
+  var linesLegend = L.mapbox.legendControl({position: 'topright'}).addLegend(document.getElementById('legenda-linhas').innerHTML);
+    linesLegend.addTo(map);
+
+  var mostraLegal = L.mapbox.legendControl({'position': 'bottomleft'}).addLegend(document.getElementById('legenda-transoceanica-benvindo').innerHTML);
+  mostraLegal.addTo(map);
+
+  map.createPane('info');
+  map.getPane('info').style.zIndex = 650;
+
   var default_bhs_line_style = {
     color: '#fd4d3d',
-    weight: 10
+    weight: 30,
+    // fillOpacity: 0.4,
+    opacity: 0.0
   }
+  var bhs_line_legends_hover = {};
+  var bhs_line_legends_click = {};
+  var bhs_line_legends_click_displayed = false;
 
   function loadBhsLineLegend(feature) {
     var template = $('#hover-bhs-line-template').html();
@@ -32,15 +49,6 @@ $( document ).ready(function() {
     $('#teste-da-bagaca').html(rendered);
   }
 
-  var bhs_line_legends_hover = {};
-  var bhs_line_legends_click = {};
-  // var cocoen_started = false;
-  var bhs_line_legends_click_displayed = false;
-  // var hoverDeBaixo = L.mapbox.legendControl({'position': 'bottomleft'})
-  // hoverDeBaixo.onAdd = function(map){
-  //   return document.getElementById('hover-de-baixo').innerHTML;
-  // };
-
   function highlightFeature(e) {
 
     map.removeControl(mostraLegal);
@@ -48,15 +56,13 @@ $( document ).ready(function() {
     if (bhs_line_legends_click_displayed === false) {
       var layer = e.target;
       layer.setStyle({
-          weight: 15,
+          weight: 30,
           color: '#FAA286',
       //     dashArray: '',
-      //     fillOpacity: 0.7
+          fillOpacity: 0.3,
+          opacity: 0.4
       });
       loadBhsLineLegend(layer.feature.properties);
-      // hoverDeBaixo.addLegend(document.getElementById('hover-de-baixo').innerHTML);
-      // hoverDeBaixo.addTo(map);
-      // map.addControl(hoverDeBaixo);
       bhs_line_legends_hover[layer.feature.properties.Id].addLegend(document.getElementById('hover-de-baixo').innerHTML);
       map.addControl(bhs_line_legends_hover[layer.feature.properties.Id]);
     }
@@ -64,9 +70,7 @@ $( document ).ready(function() {
 
   function resetHighlight(e) {
     if (bhs_line_legends_click_displayed === false) {
-    // console.log('Entrou reset!!!!');
       var layer = e.target;
-      // map.removeControl(hoverDeBaixo);
       map.removeControl(bhs_line_legends_hover[layer.feature.properties.Id]);
       layer.setStyle(default_bhs_line_style);
     }
@@ -75,49 +79,29 @@ $( document ).ready(function() {
   function showFeatureDetails(e) {
     if (bhs_line_legends_click_displayed === false) {
       bhs_line_legends_click_displayed = true;
-
       var layer = e.target;
 
       map.removeControl(bhs_line_legends_hover[layer.feature.properties.Id]);
 
       loadBhsLineBigLegend(layer.feature.properties);
-      // var legendaGiganta = L.mapbox.legendControl({'position': 'bottomleft'}).addLegend(document.getElementById('teste-da-bagaca').innerHTML);
-      // legendaGiganta.addTo(map);
       bhs_line_legends_click[layer.feature.properties.Id].addLegend(document.getElementById('teste-da-bagaca').innerHTML);
       map.addControl(bhs_line_legends_click[layer.feature.properties.Id]);
 
       $('#botao-fechar-legenda-giganta').click(function(e) {
-
         bhs_line_legends_click_displayed = false;
-        // var layer = e.target;
-          // map.removeControl(legendaGiganta);
         map.removeControl(bhs_line_legends_click[layer.feature.properties.Id]);
-
       });
-
-      // Inicia o antes e depois
-      // if (cocoen_started === false) {
+      // Liga o slider antes <-> depois
       $('.cocoen').cocoen();
-        // cocoen_started = true;
-      // }
     }
   }
 
   $.getJSON("geojson/bhs.geojson", function(bhsGeojson) {
-    L.geoJSON(bhsGeojson, {
-      style: function (feature) {
-          return default_bhs_line_style;
+    var bhs = L.geoJSON(bhsGeojson, {
+      style: {
+        color: '#fd4d3d',
+        weight: 10
       },
-      onEachFeature: function (feature, layer) {
-        layer.on({
-          mouseover: highlightFeature,
-          mouseout: resetHighlight,
-          click: showFeatureDetails
-        });
-        bhs_line_legends_hover[feature.properties.Id] = L.mapbox.legendControl({'position': 'bottomleft'});
-        bhs_line_legends_click[layer.feature.properties.Id] = L.mapbox.legendControl({'position': 'bottomleft'})
-        // layer.bindPopup(feature.properties.name);
-      }
     }).addTo(map);
   });
 
@@ -143,21 +127,23 @@ $( document ).ready(function() {
     }).addTo(map);
   });
 
-
-
-var linesLegend = L.mapbox.legendControl({position: 'topright'}).addLegend(document.getElementById('legenda-linhas').innerHTML);
-  linesLegend.addTo(map);
-
-var mostraLegal = L.mapbox.legendControl({'position': 'bottomleft'}).addLegend(document.getElementById('legenda-transoceanica-benvindo').innerHTML);
-mostraLegal.addTo(map);
-
-
-
-// var legendaGiganta = L.mapbox.legendControl({'position': 'bottomleft'}).addLegend(document.getElementById('teste-da-bagaca').innerHTML);
-// legendaGiganta.addTo(map);
-//Depois da legendaGiganta adicionada ao mapa (e, portanto, existindo), a gente acha o botão de fechar pelo id (que é mais confiável que o ego ou o superego)
-
-
+  $.getJSON("geojson/bhs.geojson", function(infoGeojson) {
+    var info = L.geoJSON(infoGeojson, {
+      style: function (feature) {
+          return default_bhs_line_style;
+      },
+      onEachFeature: function (feature, layer) {
+        layer.on({
+          mouseover: highlightFeature,
+          mouseout: resetHighlight,
+          click: showFeatureDetails
+        });
+        bhs_line_legends_hover[feature.properties.Id] = L.mapbox.legendControl({'position': 'bottomleft'});
+        bhs_line_legends_click[layer.feature.properties.Id] = L.mapbox.legendControl({'position': 'bottomleft'})
+      },
+      pane: 'info'
+    }).addTo(map);
+  });
 
   // Pontos
   $.getJSON("geojson/estacoes.geojson", function(estacoesGeojson) {
